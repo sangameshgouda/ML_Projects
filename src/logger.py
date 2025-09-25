f7a62c732bc26d985ef9d0f190eaf6055e5f4c07
sangameshgouda <sangameshgouda1@gmail.com>
2025-09-25 15:19:40 -0700

    src folder added

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+
+log_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_path, exist_ok=True)
+
+Log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+
+Log_file_path = os.path.join(log_path, Log_file)
+logging.basicConfig(
+    filename=Log_file_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)   
+
+
